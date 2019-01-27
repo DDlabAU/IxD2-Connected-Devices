a74bb8266b53234adde1d52c97bb0a555e7e27ac
Nikolaj Mikkelsen <nikolaj.c.mikkelsen@gmail.com>
2019-01-27 17:31:39 +0100

    add sketch file

---
+++ mqtt_p5js/sketch.js
@@ -0,0 +1,60 @@
+var user = 'try';
+var token = 'try';
+
+var client;
+
+
+function setup() {
+
+  // MQTT TING
+  // -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+  client = mqtt.connect('mqtt://' + user + ':' + token + '@broker.shiftr.io', {
+    clientId: 'p5js'
+  });
+
+  client.on('connect', function() {
+    console.log('client has connected!');
+
+
+    // HER SUBCRIBER VI TIL DE ADRESSER VI VIL LYTTE TIL
+    // -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+    client.subscribe('/DDLabESP/light');
+    // client.unsubscribe('/example');
+
+
+    // HVIS VI VIL PUBLISHE I FAST INTERVAL KAN VI SÆTTE DET OP HER
+    // -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+    // setInterval(function(){
+    //   client.publish('/p5jsTest', 'testing 1..2..');
+    // }, 10000);
+  });
+
+  // HER FORTÆLLER VI AT VI VIL MODTAGE BESKEDERNE I VORES FUNKTION: receiveData
+  // -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+  client.on('message', function(topic, message) {
+    receiveData(topic, message);
+  });
+
+  // -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+  // MQTT SLUT
+
+
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+}
+
+// DEN HER FUNKTION KALDER VI NÅR VI VIL SENDE EN BESKED
+function publishData(topic, message){
+    client.publish(topic, message);
+}
+
+// DEN HER FUNKTION SKYDER HVER GANG VI MODTAGER EN BESKED
+function receiveData(t, m){
+  var topic = t.toString();
+  var message = m.toString();
+
+  console.log('topic: ', topic, 'message: ', message);
+}
